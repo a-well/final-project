d1c070bd70b071ce9af8b93a8bc3307d74b7c4d2
a-well <amandaemkwellington@gmail.com>
2022-12-26 16:58:14 +0100

    clean up auth middleware

--- backend/middlewares/authenticateUser.js
+++ backend/middlewares/authenticateUser.js
@@ -2,13 +2,10 @@ import User from '../models/User'
 
 const authenticateUser = async (req, res, next) => {
   const authHeader = req.header("Authorization") || ''
-  console.log({authHeader})
   const accessToken = authHeader.replace('Bearer ', '')
-  console.log({accessToken})
   try {
     const user = await User.findOne({ accessToken })
     if (user) {
-      console.log('user found: ', user)
       req.user = user
       next()
     } else {
