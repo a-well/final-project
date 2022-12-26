2b00f98e11bc77bf784413f46c21229cf94d85a4
a-well <amandaemkwellington@gmail.com>
2022-12-26 16:58:03 +0100

    add created at to user

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
   accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString("hex")
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
   }
 })
 const User = mongoose.model("User", UserSchema)
