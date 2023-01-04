915bf0917b1b456bb85174cfda04f5d1431017e1
a-well <amandaemkwellington@gmail.com>
2023-01-04 22:09:46 +0100

    Add about

--- frontend/src/routes/About.js
+++ frontend/src/routes/About.js
@@ -3,7 +3,10 @@ import React from 'react'
 const About = () => {
   return (
     <>
-      <h1>About</h1>
+      <h1>What is this?</h1>
+      <p>Fan built thingie to help you trade Pokemons, not affiliated with PokemonGO or Nintendo</p>
+      <h2>Kudos</h2>
+      <p>Link to PokeAPI and other stuff maybe</p>
     </>
   )
 }
