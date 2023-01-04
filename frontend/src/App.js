7095b66b9ac8941e8aa8339aa01d0a8d67f9bc9f
a-well <amandaemkwellington@gmail.com>
2023-01-04 17:04:48 +0100

    Add sign up route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Layout from 'components/Layout'
+
 import Main from 'routes/Main'
 import Login from 'routes/Login'
 import NotFound from 'routes/NotFound'
+import Signup from 'routes/Signup'
 
 // import { Provider } from 'react-redux'
 // import { combineReducers, configureStore } from '@reduxjs/toolkit'
@@ -20,12 +23,18 @@ import NotFound from 'routes/NotFound'
 export const App = () => {
   return (
     // <Provider store={store}>
+
       <BrowserRouter>
-        <Routes>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/' element={<Main />}></Route>
-          <Route path='*' element={<NotFound />}></Route>
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/signup' element={<Signup />}></Route>
+            {/* Add more routes here */}
+
+            <Route path='/' element={<Main />}></Route>
+            <Route path='*' element={<NotFound />}></Route>
+          </Routes>
+        </Layout>
       </BrowserRouter>
     // </Provider>
   )
