9f7ae08de6352bbd7efb965c20f0eb13050889c0
a-well <amandaemkwellington@gmail.com>
2023-01-08 12:36:40 +0100

    Add new settings

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,18 +20,19 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     semi: 0,
     quotes: [1, 'single'],
-    "import/prefer-default-export": 0,
-    "react/prop-types": 0,
-    "no-unused-vars": 'warn',
-    "react/no-unescaped-entities": 0,
-    "react/function-component-definition": 0,
+    'import/prefer-default-export': 0,
+    'react/prop-types': 0,
+    'no-unused-vars': 0,
+    'react/no-unescaped-entities': 0,
+    'react/function-component-definition': 0,
+    'no-console': 0,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        "extensions": [".js", ".jsx"],
-        "moduleDirectory": ["src", "node_modules"]
-      }
-    }
-  }
+        extensions: ['.js', '.jsx'],
+        moduleDirectory: ['src', 'node_modules'],
+      },
+    },
+  },
 };
