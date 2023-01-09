7df569c132f67c1f256d314c9910a76b5ba1fd72
a-well <amandaemkwellington@gmail.com>
2023-01-09 22:44:33 +0100

    Update user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,6 +18,33 @@ const UserSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: () => new Date()
+  },
+  emailAddress: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  whatsApp: {
+    type: String
+  },
+  facebook: {
+    type: String
+  },
+  about: {
+    type: String
+  },
+  poGoLevel: {
+    type: String
+  },
+  poGoUsername: {
+    type: String
+  },
+  trainerCode: {
+    type: String
+  },
+  location: {
+    type: String,
+    required: true
   }
 })
 const User = mongoose.model("User", UserSchema)
