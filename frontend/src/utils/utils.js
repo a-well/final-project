313b1661a32ecbdb407bbba71f24d52d35a97658
a-well <amandaemkwellington@gmail.com>
2023-01-10 21:00:43 +0100

    test

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,5 @@
 // eslint-disable-file import/prefer-default-export
 
 console.log(process.env.REACT_APP_BASE_URL)
+
 export const API_URL = process.env.REACT_APP_BASE_URL || 'https://final-project-l47fprrrfa-lz.a.run.app'
