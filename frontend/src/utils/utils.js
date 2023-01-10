af10919839a333a037fe1053ce00a455c3f7eaa7
a-well <amandaemkwellington@gmail.com>
2023-01-10 19:28:33 +0100

    Fix api url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,3 @@
 // eslint-disable-file import/prefer-default-export
 
-const BASE_URL = process.env.BASE_URL || 'https://final-project-l47fprrrfa-lz.a.run.app'
-
-console.log('Using BASE_URL ', BASE_URL)
-export const API_URL = (slug) => `${BASE_URL}/${slug}`
+export const API_URL = process.env.BASE_URL || 'https://final-project-l47fprrrfa-lz.a.run.app'
