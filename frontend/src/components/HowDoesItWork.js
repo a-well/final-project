bbd355da4f87f98a743dc53d399f45cd319bb7c6
a-well <amandaemkwellington@gmail.com>
2023-01-10 14:53:45 +0100

    Add list

--- frontend/src/components/HowDoesItWork.js
+++ frontend/src/components/HowDoesItWork.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
-import { Button } from 'antd';
+import { Button, List, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
+
+const data = [
+  {
+    title: 'Step one',
+    description: 'Ant ddions, is refined by Ant UED Team',
+    step: '1',
+  },
+  {
+    title: 'Step two',
+    description: 'Eeeäeäeäeäeä wewe ee',
+    step: '2',
+  },
+  {
+    title: 'Step three',
+    description: 'Jasså',
+    step: '3',
+  },
+];
 
 function HowDoesItWork() {
   return (
     <div>
       <h2>How does it work?</h2>
-      <p>Like this</p>
-
-      <Link to="/signup">
-        <Button type="primary">
-          Sweet, let's go!
-        </Button>
-      </Link>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item key={item.step}>
+            <List.Item.Meta
+              avatar={<Avatar size="default">{item.step}</Avatar>}
+              title={item.title}
+              description={item.description}
+            />
+          </List.Item>
+        )}
+      />
     </div>
   );
 }
