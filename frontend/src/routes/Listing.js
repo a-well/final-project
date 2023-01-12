5b64fea8d2849273cca854a64db4801b523e71bc
a-well <amandaemkwellington@gmail.com>
2023-01-12 21:07:42 +0100

    Move buttons in listing page

--- frontend/src/routes/Listing.js
+++ frontend/src/routes/Listing.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Row, Col } from 'antd'
-import { useParams } from 'react-router-dom'
+import {
+  Row, Col, Button, Divider,
+} from 'antd'
+import { Link, useParams } from 'react-router-dom'
 import ListingItem from 'components/ListingItem'
 import useApi from 'hooks/useApi'
 import Spinner from 'components/Spinner'
+import DeleteButton from 'components/DeleteButton'
 
 function Listing() {
   const { id } = useParams()
 
   const { data: listing, isLoading } = useApi(`/api/listings/${id}`)
+  const { data: currentUser } = useApi('/api/users/me')
 
   if (!id) {
     return <div>Error: No ID parameter given</div>
@@ -18,10 +22,32 @@ function Listing() {
     return <Spinner />
   }
 
+  if (!listing) {
+    return null
+  }
+
   return (
     <Row justify="center">
       <Col xs="24">
         <ListingItem listing={listing} hoverable={false} standalone />
+
+        <div style={{ marginTop: 12 }}>
+          <Divider />
+
+          <Link to={`/users/${listing.username}`}>
+            <Button block>
+              See all listings by this user
+            </Button>
+          </Link>
+
+        </div>
+
+        {listing.username === currentUser.username && (
+          <div style={{ marginTop: 12 }}>
+            <DeleteButton id={id} />
+          </div>
+        )}
+
       </Col>
     </Row>
   )
