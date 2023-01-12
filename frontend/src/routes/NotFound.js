907ea8e1df3e73b59322e03b23ca1064f3142cf9
a-well <amandaemkwellington@gmail.com>
2023-01-12 18:10:50 +0100

    Clean up Not Found

--- frontend/src/routes/NotFound.js
+++ frontend/src/routes/NotFound.js
@@ -1,18 +1,31 @@
 import Center from 'components/Center'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Typography from 'antd/es/typography/Typography'
+import {
+  Typography, Space, Row, Col, Button, Result,
+} from 'antd'
 
-const { Title, Text } = Typography
+const { Title } = Typography
 
 function NotFound() {
   return (
-    <Center>
-      <Title>Oh no, it's a 404 *insert surprised Pikachu*</Title>
-      <Center>
-        <Link to="/">Go home</Link>
-      </Center>
-    </Center>
+    <Space
+      direction="vertical"
+      style={{
+        width: '100%',
+      }}
+    >
+      <Result
+        status="error"
+        title="Error 404: Page not found"
+        subTitle="Must be the work of Team Rocket..."
+        extra={[
+          <Link to="/home">
+            <Button type="primary" key="console">Go to home page</Button>
+          </Link>,
+        ]}
+      />
+    </Space>
   )
 }
 
